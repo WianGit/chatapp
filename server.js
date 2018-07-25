bea6ff801b37ae20a7284d3076ca06c0a075de90
wian <wian@neuco.co.za>
2018-07-25 10:15:51 +0200

    api
    api

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app =express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+
+
+var engine = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
+
+
 users = [];
 connections = [];
 
@@ -9,9 +18,19 @@ server.listen(process.env.PORT || 3000);
 
 console.log('server running');
 
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api', function (req, res) {
+    console.log( req.query.id);
+    var id = '';
+    if(req.query.id === undefined){
+        id=false;
+    }else{
+        id = req.query.id;
+    }
+    res.render(__dirname +'/views/api.html',{id:id});
 });
 
 
@@ -31,6 +50,13 @@ io.sockets.on('connection' , function (socket) {
     socket.on('send message', function (data) {
         console.log(data);
         io.sockets.emit('new message', {msg:data, user:socket.username});
+    });
+
+
+    //  -------------------------------------------------------------- received APIs
+    socket.on('send api', function (data) {
+        console.log(data);
+        io.sockets.emit('new api', {id:data});
     });
 
 
